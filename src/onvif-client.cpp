b56dc928da3d174c19ec9c7711f929c9d00755ca
tuananh-lab <82093856+tuananh-lab@users.noreply.github.com>
2024-07-01 09:03:23 +0700

    Update onvif-client to send service requests to port 8080

--- src/onvif-client.cpp
+++ src/onvif-client.cpp
@@ -1,22 +1,3 @@
-/* --------------------------------------------------------------------------
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
-
-	You should have received a copy of the GNU General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
------------------------------------------------------------------------------
- client.cpp
-
- onvif client
------------------------------------------------------------------------------ */
-
 #include <memory>
 #include <sstream>
 #include <thread>
@@ -152,7 +133,7 @@ std::string getLocalIp()
 
 int main(int argc, char *argv[])
 {
-	std::string url = "http://127.0.0.1:8080";
+	std::string url = "http://127.0.0.1:8080";	//By default onvif-client is sending requests to the server on port 8080
 	std::string username;
 	std::string password;
 	int c = 0;
