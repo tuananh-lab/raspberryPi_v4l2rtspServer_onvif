36f7d77cfb0243af2d1002890f698fe9b28c6a45
anhtt <anhtt@hsptek.com>
2024-07-02 13:55:20 +0700

    Update explain hanlde event notification

--- src/serverEvent.cpp
+++ src/serverEvent.cpp
@@ -1,27 +1,8 @@
-/* --------------------------------------------------------------------------
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
------------------------------------------------------------------------------
- serverEvent.cpp
- 
- events.wsdl server
------------------------------------------------------------------------------ */
-
+/*Here implement an ONVIF-compliant event handling web service. 
+It includes functionalities such as retrieving service capabilities, creating pull point subscriptions for event notifications, and obtaining event properties.Manage event-based interactions between devices and clients, supporting real-time event notifications and configurations.*/
 #include <sstream>
-
 #include "soapEventBindingService.h"
 #include "onvif_impl.h"
-
 
 int EventBindingService::GetServiceCapabilities(_tev__GetServiceCapabilities *tev__GetServiceCapabilities, _tev__GetServiceCapabilitiesResponse &tev__GetServiceCapabilitiesResponse) 
 {
