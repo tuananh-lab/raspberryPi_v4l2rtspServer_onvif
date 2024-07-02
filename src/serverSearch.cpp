ee3b06b17c0dc7ffb2ec30a161f727c60e0fdcb7
tuananh-lab <82093856+tuananh-lab@users.noreply.github.com>
2024-07-02 14:34:36 +0700

    Update explain search service such as:recording,events,metadata,..

--- src/serverSearch.cpp
+++ src/serverSearch.cpp
@@ -1,16 +1,6 @@
 /* --------------------------------------------------------------------------
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+Here implements various ONVIF search service operations, including capabilities retrieval and different types of searches 
+such as recordings, events, PTZ positions, and metadata, along with state management for ongoing searches.
 -----------------------------------------------------------------------------
  serverSearch.cpp
  
